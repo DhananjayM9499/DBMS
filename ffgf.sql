1010471d64fffb3ed5eb3f8896c7560ed4d1d41a
Dhananjay <dmmohokar94@gmail.com>
2022-12-22 20:57:44 +0530

    files renamed

--- ffgf.sql
+++
@@ -1,9 +0,0 @@
-create database PGDAC2022;
-use PGDAC2022;
-create table enrolnment
-(
-PRN integer(12),
-studentname varchar(20)
-);
-insert into PGDAC2022.enrolnment values(2522000,'danny');
-select*from enrolnment;
